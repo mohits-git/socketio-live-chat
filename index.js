75ee6eb9a19790325e6879f4b64431d6b664a292
mohits-git <mohitkhatri2805@gmail.com>
2024-05-05 19:12:58 +0530

    feat: scalable chat application with @socket.io/clusters-adapter, handling disconnections, and proper broadcasting of events

--- index.js
+++ index.js
@@ -2,26 +2,92 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const { Server } = require('socket.io');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
+const { availableParallelism } = require('node:os');
+const cluster = require('node:cluster');
+const { createAdapter, setupPrimary } = require('@socket.io/cluster-adapter')
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+if(cluster.isPrimary) {
+    const totalCPUs = availableParallelism();
 
-//Socket io handler
-io.on('connection', (socket) => {
-    console.log('A new user connected', socket.id);
-    socket.on('user-message', (message)=> {
-        console.log('A new user message', message);
-        io.emit('message', message);
+    for(let i = 0; i < totalCPUs; i++) {
+        cluster.fork({
+            PORT: 3000 + i
+        });
+    }
+
+    return setupPrimary();
+}
+
+async function main() {
+
+    const db = await open({
+        filename: "chat.db",
+        driver: sqlite3.Database
     });
-});
 
-app.use(express.static(path.resolve('./public')));
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            client_offset TEXT UNIQUE,
+            content TEXT
+         );
+        `)
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html');
-});
+    const app = express();
+    const server = http.createServer(app);
+    const io = new Server(server, {
+        connectionStateRecovery: {},
+        adapter: createAdapter()
+    });
 
-server.listen(3000, () => {
-    console.log('Server running on port 3000');
-});
+    io.on('connection', async (socket) => {
+        console.log('A new user connected', socket.id);
+        socket.on('disconnect', () => console.log("User disconnected."));
+
+        socket.on('chat message', async (message, clientOffset, callback) => {
+            let result;
+            try {
+                result = await db.run(`INSERT INTO messages (content, client_offset) VALUES (?, ?)`, message, clientOffset);
+            } catch (error) {
+                if (error.errno === 19) {
+                    callback();
+                } else {
+                    io.to(socket.id).emit('error', "Could not send your message");
+                    console.log(error);
+                }
+                return;
+            }
+            io.emit('chat message', message, result.lastID);
+            callback();
+        });
+
+        if (!socket.recovered) {
+            try {
+                await db.each(`SELECT id, content FROM messages WHERE id > ?`,
+                    [socket.handshake.auth.serverOffset || 0],
+                    (_err, row) => {
+                        socket.emit('chat message', row.content, row.id);
+                    }
+                );
+            } catch (error) {
+                io.to(socket.id).emit('error', "Could not get the message");
+                return;
+            }
+        }
+    });
+
+    app.use(express.static(path.resolve('./public')));
+
+    app.get('/', (req, res) => {
+        res.sendFile('index.html');
+    });
+
+    const port = process.env.PORT;
+    server.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
+
+main();
